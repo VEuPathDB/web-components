284237a8fca17a8c95eeb9c1d3b7cd1a5e11a718
linxu123-git <linxu123@upenn.edu>
2020-07-14 17:38:35 -0400

    make type optinal in PlotlyPlot and add Bar Chart with Line Plot

---
+++ src/plots/HorizontalBarChartWithLinePlot.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import HorizontalBarCahrtWithLinePlot from './HorizontalBarChartWithLinePlot';
+
+
+export default {
+    title: 'HorizontalBarCahrtWithLinePlot',
+    component: HorizontalBarCahrtWithLinePlot,
+  };
+  
+
+const x1 = [1.3586, 2.2623, 4.9821, 6.5096, 7.4812, 7.5133, 15.2148, 17.5204];
+const y1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+//const y1 = ['Brazil', 'Peru', 'S.Asia', 'Africa', 'India', 'Mali', 'Namibia', 'Uganda'];
+const x2 = [93453, 81666, 69889, 78381, 141395, 92969, 66090, 122379];
+const y2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+//const y2 = ['Brazil', 'Peru', 'S.Asia', 'Africa', 'India', 'Mali', 'Namibia', 'Uganda'];
+
+
+
+export const BarChartWithLinePlot = () => <HorizontalBarCahrtWithLinePlot
+//onPlotUpdate={action('state updated')}
+data={
+  {
+    barX: x1,
+    barY: y1,
+    lineX: x2,
+    lineY: y2
+    
+  }
+
+  }
+
+plotTitle="Rate and Count"
+
+
+
+/>
---
+++ src/plots/HorizontalBarChartWithLinePlot.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import PlotlyPlot from "./PlotlyPlot";
+import { PlotComponentProps } from "./Types";
+import { Layout, Annotations } from "plotly.js";
+
+
+
+interface HorizontalBarChartWithLinePlotDatum {
+  barX: number[];
+  barY: string[];
+  lineX: number[];
+  lineY: string[];
+}
+interface Props {
+  data: HorizontalBarChartWithLinePlotDatum;
+  plotTitle: string;
+}
+
+export default function HorizontalBarChartWithLinePlot(props: Props) {
+  const {plotTitle, data} = props;
+  const layout :Partial<Layout>= {
+    title: {
+        text: plotTitle
+    },
+    xaxis:{
+        range: [0, 20],
+        domain: [0, 0.45],
+        zeroline: false,
+        showline: true,
+        showticklabels: true,
+        showgrid: true
+    },
+    xaxis2: {
+        range: [25000, 150000],
+        domain: [0.55, 1],
+        zeroline: false,
+        showline: true,
+        showticklabels: true,
+        showgrid: true,
+        tick0: 250000,
+        dtick: 25000
+    },
+    legend: {
+        x: 0.029,
+        y: 1.2,
+        font: {
+          size: 10
+        }
+      },
+    margin: {
+        l: 100,
+        r: 20,
+        t: 200,
+        b: 70
+      },
+    width: 600,
+    height: 600,
+    paper_bgcolor: 'rgb(248,248,255)',
+    plot_bgcolor: 'rgb(248,248,255)',
+
+   annotations: []
+    
+  };
+
+
+
+  for ( var i = 0 ; i < data.barX.length ; i++ ) {
+
+    var result : Partial<Annotations> = {
+      xref: 'x',
+      yref: 'y',
+      x: data.barX[i]+2.3,
+      y: data.barY[i],
+      text: data.barX[i] + '%',
+      font: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(50, 171, 96)'
+      },
+       showarrow: false,
+    };
+    var result2 : Partial<Annotations> = {
+      xref: 'x2' as 'x',
+      yref: 'y',
+      x: data.lineX[i] - 25000,
+      y: data.lineY[i],
+      text: data.lineX[i]/1000 + ' K',
+      font: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(128, 0, 128)'
+      },
+       showarrow: false
+    };
+    layout.annotations!.push(result, result2) ;
+  }
+
+ var trace1={x:data.barX, y:data.barY, xaxis: 'x',yaxis: 'y', type:'bar', orientation: 'h'}
+ var trace2={x:data.lineX, y:data.lineY,xaxis: 'x2',yaxis: 'y'}
+
+
+  return (
+  <
+    PlotlyPlot data={[trace1,trace2]}  layout={layout} 
+    />
+  )
+}
